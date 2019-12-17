ac173360d5c77abb305a1ab0bdfa1891db385ed2
Basha <basha16an@gmail.com>
2019-12-17 06:55:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
-variable "testing_variable" {
+variable "aws_region_name" {
   type=string
-  default="testing_value"
+  default="us-east-1"
 }
 
-output "test_output" {
-  value=var.testing_variable
+variable "aws_instance_name" {
+
+  type=map
+  default= {
+    us-east-1="EastRegion_Dev_Instance"
+    us-west-1="WestRegion_Dev_Instance"
+    us-south-1="SouthRegion_Dev_Instance"
+    }
   }
+output "aws_server_name" {
+  value="${lookup(var.aws_instance_name , var.aws_region_name)}"
+  }
+
